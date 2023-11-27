58ce140a61be31d85ba3327054309695736db380
Venicius Vieira <veniciusvieira90@gmail.com>
2023-11-27 12:34:51 -0300

    Adicionado o  arquivo if_elif_else.py

---
+++ conteudo_artigos/if_elif_else.py
@@ -0,0 +1,22 @@
+# Estruturas condionais condiçoes false e true
+# IF
+media = 7
+if media > 6.9:
+ print("Você foi aprovado")
+
+# IF, ELSE
+media = 5
+if media < 4.9:
+ print("Você foi reprovado")
+else:
+    print("Você foi aprovado")
+
+# IF, ELIF, ELSE
+media = 6
+if media < 5:
+  print("Você foi reprovado")
+elif media > 5: 
+    media < 7
+    print("Você fará a recuperação")
+else:
+    print("Você foi aprovado")
