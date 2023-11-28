84b2fc71b6ddfc38794eb653a9da84e67d842550
Venicius Vieira <veniciusvieira90@gmail.com>
2023-11-28 12:05:39 -0300

    Adicionado o arquivo primeiro_exercicio.py

---
+++ primeiro_exercicio.py
@@ -0,0 +1,40 @@
+# Para usar o Python online digite pythonrepl no navegador
+print("Olá aluno! Bem vindo!")
+print("Olá Mundo!")
+# help() - pode ser usada no console do Python, função que lista informações
+# sep e end no print
+print("O Brasil é um pais bem grande!")
+print("O", "Brasil", "é", "um", "pais", "bem", "grande!", sep="-")
+print("O", "Brasil", "é", "um", "pais", "bem", "grande!", end="END\n")
+pais = "México"
+print(type(pais))
+print(f'O {pais} é um pais bem grande!')
+palavra1 = "Bom"
+palavra2 = "dia"
+palavra3 = "a todos"
+print(palavra1, palavra2, palavra3, sep=" ", end="!\n")
+# mais exemplos
+subst = "Python"
+verbo = "é"
+adjetivo = "fantástico"
+print(subst, verbo, adjetivo, sep="_", end="!\n")
+# mais exemplos
+dia = 15
+mes = 10
+ano = 2015
+print(dia, mes, ano, sep="/")
+# classes dados
+pais = "Brasil"
+print(type(pais))
+pais = 644
+print(type(pais))
+preco = 49.99
+print(type(preco))
+# snake case exemplo
+idade_esposa = 20
+perfil_vip = 'Flávio Almeida'
+recibos_em_atraso = 30
+#  padrão não utilizado no Python
+idadeEsposa = 20
+perfilVip = 'Flávio Almeida'
+recibosEmAtraso = 30
