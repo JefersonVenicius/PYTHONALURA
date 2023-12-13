28404e8d86e4fe6aa3cf0dc972f74fbb26de51f1
Venicius Vieira <veniciusvieira90@gmail.com>
2023-12-13 10:39:32 -0300

    Adicionado alterações nos arquivos

--- jogos/forca.py
+++ jogos/forca.py
@@ -4,4 +4,5 @@ def jogar():
     print("*****Bem vindo ao jogo de Forca!*****")
     print("*************************************")
 
-    print("Fim do jogo")
+if __name__ == "__main__":
+    jogar()
