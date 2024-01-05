bc389c6822f2970cca72db5c4d6adc5daab7963a
Venicius Vieira <veniciusvieira90@gmail.com>
2024-01-05 16:09:35 -0300

    Adicionado conteudo

--- jogos/forca.py
+++ jogos/forca.py
@@ -4,11 +4,12 @@ def jogar():
     print("*****Bem vindo ao jogo de Forca!*****")
     print("*************************************")
 
-    palavra_secreta = "python"
+    palavra_secreta = "python".upper()
     letras_acertadas = ["_", "_", "_", "_", "_", "_"]
         
     enforcou = False
     acertou = False
+    erros = 0
 
     print(letras_acertadas)
    
@@ -19,12 +20,23 @@ def jogar():
         
         index = 0
         for letra in palavra_secreta:
-            if chute.upper == letra.upper:
+            if chute.upper() == letra.upper():
                 letras_acertadas[index] = letra
-            index = index + 1
+            index += 1
         
-        print(letras_acertadas)
+        else:
+            erros += 1
 
+        enforcou = erros == 6
+        acertou = "_" not in letras_acertadas
+        print(letras_acertadas)
+    
+    if(acertou):
+        print("Você ganhou!!!")
+    
+    else:
+        print("Você perdeu!!!")
+    
     print("Fim do Jogo")
 
 if __name__ == "__main__":
