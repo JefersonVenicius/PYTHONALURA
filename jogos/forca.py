6389f714c64bfa70fad01d706e9b368ce8a2f8a3
Venicius Vieira <veniciusvieira90@gmail.com>
2024-01-05 13:47:26 -0300

    Adicionado conteudo no arquivo forca.py

--- jogos/forca.py
+++ jogos/forca.py
@@ -4,13 +4,26 @@ def jogar():
     print("*****Bem vindo ao jogo de Forca!*****")
     print("*************************************")
 
-    palavra_secreta = "banana"
+    palavra_secreta = "python"
     enforcou = False
     acertou = False
    
     while(not enforcou and not acertou):
+        
+        chute = input("Qual letra? ")
+        chute = chute.strip()
+        
+        index = 0
+        
+        for letra in palavra_secreta:
+            if chute.upper == letra.upper:
+                print("Encontrei a letra {} na posição {}".format(letra, index))
+            index = index + 1
+           
         print("Jogando...")
+
     
+
     print("Fim do Jogo")
 
 if __name__ == "__main__":
